ce7dbb2e9bdcba2b7e327446faa4ff92042da69d
Simon Dao <bobross1005@gmail.com>
2019-03-14 20:57:18 -0700

    Initial commit :wq

---
+++ Demo/src/demo/Demo.java
@@ -0,0 +1,54 @@
+
+package demo;
+
+import java.awt.event.ActionListener;
+import java.util.Random;
+import javax.swing.*;
+
+
+ public class Demo  {
+ 
+
+    
+public static void main(String[] args){
+    
+boolean hi = true;
+
+int hello = 5;
+while(hi){     
+hello++;
+       
+      
+    System.out.println(hello);
+    }
+}
+      
+ }        
+
+    
+    
+       
+     
+ 
+ 
+       
+
+    
+       
+    
+       
+ 
+    
+    
+
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+ 
