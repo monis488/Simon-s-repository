a5eb0d1314bec7f303f6fcefab28bf83280a397d
Simon Dao <bobross1005@gmail.com>
2019-05-27 12:57:13 -0700

    hello

--- LWJGL/src/Main_Package/Main.java
+++ LWJGL/src/Main_Package/Main.java
@@ -1,73 +1,59 @@
 package Main_Package;
+
 import static org.lwjgl.glfw.GLFW.*;
 import static org.lwjgl.opengl.GL11.*;
 import org.lwjgl.opengl.GL;
 
-
-
 public class Main {
 
-	
-	
-public Main() {
-	
-	if(!glfwInit()) {
-		
-		System.err.println("testing");
-		System.exit(1);
-		
-		}
-	
-	long win = glfwCreateWindow(640,480,"LWJGL GAME",0,0);
-		
-	glfwShowWindow(win);
-	glfwMakeContextCurrent(win);
-	GL.createCapabilities();
-	
-	//temp
-	float x = 0;
-	float y = 0;
-	
-	float red = 1;
-	float blue =1;
-	//temp
-	
-	while(!glfwWindowShouldClose(win)) {
-		
-		glfwPollEvents();
-		
-	//////////////////////////////////////////////////	
-		glClear(GL_COLOR_BUFFER_BIT);
-		glBegin(GL_QUADS);
-		
-		if(glfwGetKey(win,GLFW_KEY_W) == GLFW_TRUE) { y+=.0001f; }
-		if(glfwGetKey(win,GLFW_KEY_A) == GLFW_TRUE) { x-=.0001f; }
-		if(glfwGetKey(win,GLFW_KEY_S) == GLFW_TRUE) { y-=.0001f; }
-		if(glfwGetKey(win,GLFW_KEY_D) == GLFW_TRUE) { x+=.0001f; }
-		if(glfwGetKey(win,GLFW_KEY_F) == GLFW_TRUE) { glColor4f(red,1,blue,1); }
-		
-		glColor4f(red,0,blue,0);
-		glVertex2f(-0.5f+x,0.5f+y);
-		
-		
-		glVertex2f(0.5f +x,0.5f+y);
-		
-		
-		glVertex2f(0.5f +x,-0.5f +y);
-		
-		
-		glVertex2f(-0.5f +x,-0.5f +y);
-		glEnd();
-		
-	/////////////////////////////////////////////////	
-		glfwSwapBuffers(win);
-		
-	}
-}
-		public static void main(String [] args) {
-			new Main();
-		}
-	
-	
-}
+	/*
+	 * In the constructor we first create a window called win and make it 640 * 480
+	 * pixels with the title of "LWJGL GAME" The constructor then shows it
+	 * We then create a game loop 
+	 * Also by pressing escape, the window will be destroyed and the loop 
+	 * will stop running terminating the program
+	 * The constructor will be initialized in the main function
+	 * 
+	 */
 
+	public Main() {
+
+		if (!glfwInit()) {
+
+			System.err.println("testing");
+			System.exit(1);
+
+		}
+
+		long win = glfwCreateWindow(640, 480, "LWJGL GAME", 0, 0);
+
+		glfwShowWindow(win);
+		glfwMakeContextCurrent(win);
+		GL.createCapabilities();
+
+		//GAME LOOP!!!!
+		while (!glfwWindowShouldClose(win)) {
+
+			glfwPollEvents();
+
+			//////////////////////////////////////////////////
+			glClear(GL_COLOR_BUFFER_BIT);
+			glBegin(GL_QUADS);
+			glfwSwapBuffers(win);
+
+			if (glfwGetKey(win, GLFW_KEY_ESCAPE) == GLFW_TRUE) {
+
+				glfwDestroyWindow(win);
+				break;
+
+			}
+		}
+		/////////////////////////////////////////////////
+
+	}
+
+	public static void main(String[] args) {
+		new Main();
+	}
+
+}
---
+++ LWJGL/src/Main_Package/Texture.java
@@ -0,0 +1,76 @@
+package Main_Package;
+
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import javax.imageio.ImageIO;
+import org.lwjgl.BufferUtils;
+import static org.lwjgl.opengl.GL11.*;
+
+public class Texture {
+
+	int id;
+	int width, height;
+
+	/**
+	 * Input the path of the file into the constructor The method will then find the
+	 * image file then find the files width and height after that the method will
+	 * create an array called pixels_raw that can store RGB integers for every pixel
+	 * in the image Then 2 for loops will loop through all of the pixels and find
+	 * the RGB of every pixel and store them inside the pixels_raw array
+	 * 
+	 * @param filename
+	 */
+
+	public Texture(String filename) {
+
+		BufferedImage bi;
+
+		try {
+			bi = ImageIO.read(new File(filename));
+			width = bi.getWidth();
+			height = bi.getHeight();
+
+			int[] pixels_raw = new int[width * height * 4];
+			pixels_raw = bi.getRGB(0, 0, width, height, null, 0, width);
+
+			ByteBuffer pixels = BufferUtils.createByteBuffer(width * height * 4);
+
+			for (int i = 0; i < height; i++) {
+				for (int j = 0; j < width; j++) {
+					int pixel = pixels_raw[i * width + j];
+					pixels.put((byte) ((pixel >> 16) & 0xFF)); // RED
+					pixels.put((byte) ((pixel >> 8) & 0xFF)); // GREEN
+					pixels.put((byte) (pixel & 0xFF)); // BLUE
+					pixels.put((byte) ((pixel >> 24) & 0xFF));// ALPHA
+				}
+			}
+
+			pixels.flip();
+
+			id = glGenTextures();
+
+			glBindTexture(GL_TEXTURE_2D, id);
+
+			glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+			glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+
+			glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_BYTE, pixels);
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+	}
+	
+	/*
+	 * This method will give that specific texture an id so 
+	 * could be called upon more easily
+	 * 
+	 */
+	
+	public void bind() {
+		glBindTexture(GL_TEXTURE_2D, id);
+	}
+}
